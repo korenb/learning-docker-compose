1eb22baa5e811273ae4e0d929ff504bd82fcaf6f
Sergey Korenyugin <korenb23@mail.ru>
2018-02-27 18:50:22 +0500

    fuck it

--- test-app/Controllers/ValuesController.cs
+++ test-app/Controllers/ValuesController.cs
@@ -9,36 +9,16 @@ namespace test_app.Controllers
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
-        // GET api/values
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "Вроде", "всё", "работает" };
         }
-
-        // GET api/values/5
+        
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            return "Вот! глянь текущее время: " + DateTime.Now;
         }
     }
 }
--- test-app/Program.cs
+++ test-app/Program.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace test_app
 {
@@ -20,6 +13,7 @@ namespace test_app
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                //.UseUrls("http://0.0.0.0:5000/")
                 .Build();
     }
 }
