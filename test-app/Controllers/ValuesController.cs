90ca2881ddddbd29049aa3611cd48e5aca0af9d3
Sergey Korenyugin <korenb23@mail.ru>
2018-03-01 15:27:29 +0500

    added build spec for AWS code pipeline

--- test-app/Controllers/ValuesController.cs
+++ test-app/Controllers/ValuesController.cs
@@ -13,14 +13,14 @@ namespace test_app.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value222222222" };
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "value";
+            return "current date time: " + DateTime.Now;
         }
 
         // POST api/values
